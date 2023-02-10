ab29c594c0bc5b134f7d1cd01ee8b5414b876985
namrata.t <prachi.easternts@gmail.com>
2023-02-10 14:35:07 +0530

    pagewise data fetched from API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,7 @@
+<!-- eslint-disable prettier/prettier -->
+<!-- eslint-disable prettier/prettier -->
+<!-- eslint-disable prettier/prettier -->
+<!-- eslint-disable prettier/prettier -->
 <!-- eslint-disable vue/require-v-for-key -->
 <!-- eslint-disable vue/no-unused-vars -->
 <!-- eslint-disable vue/valid-v-slot -->
@@ -8,8 +12,10 @@
       :headers="headers"
       :items="passengers"
       sort-by="trips"
-      server-items-length="66586"
+      :server-items-length=totalPassengers
       class="elevation-1"
+      :options.sync="options"
+      :items-per-page="10"
     >
       <template v-slot:item.airline="{ item }">
         <span v-for="name in item.airline" v-bind="item.airline.id">
@@ -69,9 +75,7 @@
           </v-dialog>
           <v-dialog v-model="dialogDelete" max-width="500px">
             <v-card>
-              <v-card-title class="text-h5"
-                >Are you sure you want to delete this item?</v-card-title
-              >
+              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" text @click="closeDelete"
@@ -97,12 +101,19 @@
   </v-container>
 </template>
 <script>
+import axios from "axios";
 export default {
   data: () => ({
     dialog: false,
     dialogDelete: false,
     name: "",
     airline: " ",
+    // eslint-disable-next-line prettier/prettier
+    options:{},
+    loading: true,
+    passengers: [],
+    totalPassengers: 0,
+    page: 0,
     headers: [
       {
         text: "Airline Name",
@@ -115,8 +126,9 @@ export default {
 
       { text: "Actions", value: "actions", sortable: false },
     ],
-    passengers: [],
+
     editedIndex: -1,
+
     editedItem: {
       name: "",
       trips: 0,
@@ -143,6 +155,13 @@ export default {
     dialogDelete(val) {
       val || this.closeDelete();
     },
+    options: {
+      handler() {
+        this.page += 1;
+        this.getDataFromApi();
+      },
+      deep: true,
+    },
   },
 
   created() {
@@ -151,309 +170,100 @@ export default {
 
   methods: {
     initialize() {
-      this.passengers = [
-        {
-          _id: "63e34c2d87ade9831e0cbd69",
-          name: "Harish",
-          trips: 230,
-          airline: [
-            {
-              id: 4,
-              name: "Emirates",
-              country: "Dubai",
-              logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/150px-Emirates_logo.svg.png",
-              slogan: "From Dubai to destinations around the world.",
-              head_quaters: "Garhoud, Dubai, United Arab Emirates",
-              website: "www.emirates.com/",
-              established: "1985",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade91cd70cbd6d",
-          name: "RAJESH",
-          trips: 234,
-          airline: [
-            {
-              id: 5,
-              name: "Eva Air",
-              country: "Taiwan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
-              slogan: "Sharing the World, Flying Together",
-              head_quaters:
-                "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
-              website: "www.evaair.com",
-              established: "1989",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade912620cbd6f",
-          name: "AJITHA",
-          trips: 345,
-          airline: [
-            {
-              id: 9,
-              name: "Japan Airlines",
-              country: "Japan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Japan_Airlines_Logo_%282011%29.svg/300px-Japan_Airlines_Logo_%282011%29.svg.png",
-              slogan: "Fly into tomorrow",
-              head_quaters: "Shinagawa, Tokyo, Japan",
-              website: "www.jal.com",
-              established: "1951",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade9b1030cbd72",
-          name: "Harish",
-          trips: 230,
-          airline: [
-            {
-              id: 4,
-              name: "Emirates",
-              country: "Dubai",
-              logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/150px-Emirates_logo.svg.png",
-              slogan: "From Dubai to destinations around the world.",
-              head_quaters: "Garhoud, Dubai, United Arab Emirates",
-              website: "www.emirates.com/",
-              established: "1985",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade911bd0cbd76",
-          name: "AJIT",
-          trips: 345,
-          airline: [
-            {
-              id: 9,
-              name: "Japan Airlines",
-              country: "Japan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Japan_Airlines_Logo_%282011%29.svg/300px-Japan_Airlines_Logo_%282011%29.svg.png",
-              slogan: "Fly into tomorrow",
-              head_quaters: "Shinagawa, Tokyo, Japan",
-              website: "www.jal.com",
-              established: "1951",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade932fd0cbd78",
-          name: "RAJESH",
-          trips: 234,
-          airline: [
-            {
-              id: 5,
-              name: "Eva Air",
-              country: "Taiwan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
-              slogan: "Sharing the World, Flying Together",
-              head_quaters:
-                "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
-              website: "www.evaair.com",
-              established: "1989",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade943f10cbd7b",
-          name: "Harish",
-          trips: 230,
-          airline: [
-            {
-              id: 4,
-              name: "Emirates",
-              country: "Dubai",
-              logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/150px-Emirates_logo.svg.png",
-              slogan: "From Dubai to destinations around the world.",
-              head_quaters: "Garhoud, Dubai, United Arab Emirates",
-              website: "www.emirates.com/",
-              established: "1985",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade90baf0cbd7f",
-          name: "RAJESH",
-          trips: 234,
-          airline: [
-            {
-              id: 5,
-              name: "Eva Air",
-              country: "Taiwan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
-              slogan: "Sharing the World, Flying Together",
-              head_quaters:
-                "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
-              website: "www.evaair.com",
-              established: "1989",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34c2e87ade969e30cbd81",
-          name: "AJITHA",
-          trips: 345,
-          airline: [
-            {
-              id: 9,
-              name: "Japan Airlines",
-              country: "Japan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Japan_Airlines_Logo_%282011%29.svg/300px-Japan_Airlines_Logo_%282011%29.svg.png",
-              slogan: "Fly into tomorrow",
-              head_quaters: "Shinagawa, Tokyo, Japan",
-              website: "www.jal.com",
-              established: "1951",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e34e0187ade954e10cbe18",
-          name: "Prashant",
-          trips: 25,
-          airline: [
-            {
-              id: 5,
-              name: "Eva Air",
-              country: "Taiwan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
-              slogan: "Sharing the World, Flying Together",
-              head_quaters:
-                "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
-              website: "www.evaair.com",
-              established: "1989",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e4d663787533f77acb12c8",
-          name: "testuser",
-          trips: 15,
-          airline: [
-            {
-              id: 10,
-              name: "Swiss International Air Lines",
-              country: "Switzerland",
-              logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Swiss_International_Air_Lines_Logo_2011.svg/200px-Swiss_International_Air_Lines_Logo_2011.svg.png",
-              slogan: "Made of Switzerland. The Airline of Switzerland",
-              head_quaters:
-                "EuroAirport Basel Mulhouse Freiburg near Basel, Switzerland",
-              website: "www.swiss.com",
-              established: "2002",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e4d6657875338ccccb12cd",
-          name: "testupName",
-          trips: 10,
-          airline: [
-            {
-              id: 7,
-              name: "Deutsche Lufthansa AG",
-              country: "Germany",
-              logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Lufthansa_Logo_2018.svg/300px-Lufthansa_Logo_2018.svg.png",
-              slogan: "Say yes to the world",
-              head_quaters: "Cologne, Germany",
-              website: "lufthansa.com",
-              established: "1953",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e4d6687875335e69cb12d4",
-          name: "testupdatedName",
-          trips: 3,
-          airline: [
-            {
-              id: 1,
-              name: "Quatar Airways",
-              country: "Quatar",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/300px-Qatar_Airways_Logo.svg.png",
-              slogan: "Going Places Together",
-              head_quaters: "Qatar Airways Towers, Doha, Qatar",
-              website: "www.qatarairways.com",
-              established: "1994",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e4d66a78753322aecb12db",
-          name: "",
-          trips: 10,
-          airline: [
-            {
-              id: 8,
-              name: "Thai Airways",
-              country: "Thailand",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Thai_Airways_Logo.svg/200px-Thai_Airways_Logo.svg.png",
-              slogan: "Smooth as Silk / I Fly THAI",
-              head_quaters:
-                "Jom Phol Subdistrict, Chatuchak, Bangkok, Thailand",
-              website: "www.thaiairways.com",
-              established: "1960",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e4d6707875334a98cb12e5",
-          name: "TestUserName",
-          trips: 3,
-          airline: [
-            {
-              id: 5,
-              name: "Eva Air",
-              country: "Taiwan",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/EVA_Air_logo.svg/250px-EVA_Air_logo.svg.png",
-              slogan: "Sharing the World, Flying Together",
-              head_quaters:
-                "376, Hsin-Nan Rd., Sec. 1, Luzhu, Taoyuan City, Taiwan",
-              website: "www.evaair.com",
-              established: "1989",
-            },
-          ],
-          __v: 0,
-        },
-        {
-          _id: "63e4d67378753347efcb12ea",
-          name: "TestUserName12",
-          trips: 1,
-          airline: [
-            {
-              id: 3,
-              name: "Cathay Pacific",
-              country: "Hong Kong",
-              logo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Cathay_Pacific_logo.svg/300px-Cathay_Pacific_logo.svg.png",
-              slogan: "Move Beyond",
-              head_quaters:
-                "Cathay City, Hong Kong International Airport, Chek Lap Kok, Hong Kong",
-              website: "www.cathaypacific.com",
-              established: "1946",
-            },
-          ],
-          __v: 0,
-        },
-      ];
+      this.passengers = [];
+    },
+    getDataFromApi() {
+      this.loading = true;
+      this.getPassengersData();
+    },
+    async getPassengersData() {
+      await axios
+        .get(
+          `https://api.instantwebtools.net/v1/passenger?size=10&page=` +
+            this.page
+        )
+        .then((response) => {
+          this.passengers = response.data.data;
+          this.totalPassengers = response.data.totalPassengers;
+          this.totalPages = response.data.totalPages;
+          console.log("Total number of passengers is: " + this.totalPassengers);
+        })
+        .catch((error) => console.log(error));
+      console.log(
+        "Passenger data fetched from API is: " + JSON.stringify(this.passengers)
+      );
+
+      for (var i = 0; i < this.passengers.length; i++) {
+        this.airlineName = this.passengers[i].airline;
+        this.name = this.passengers[i].name;
+        console.log(this.name);
+        console.log(this.airlineName);
+
+        for (var x in this.airlineName) {
+          this.airline = this.airlineName[x].name;
+          console.log(this.airline);
+        }
+      }
+    },
+    fakeApiCall() {
+      return new Promise((resolve) => {
+        const { sortBy, sortDesc, page, itemsPerPage } = this.options;
+
+        let items = this.getPassengersData();
+        const total = items.length;
+
+        if (sortBy.length === 1 && sortDesc.length === 1) {
+          items = this.passengers.sort((a, b) => {
+            const sortA = a[sortBy[0]];
+            const sortB = b[sortBy[0]];
+
+            if (sortDesc[0]) {
+              if (sortA < sortB) return 1;
+              if (sortA > sortB) return -1;
+              return 0;
+            } else {
+              if (sortA < sortB) return -1;
+              if (sortA > sortB) return 1;
+              return 0;
+            }
+          });
+        }
+
+        if (itemsPerPage > 0) {
+          items = items.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+        }
+
+        setTimeout(() => {
+          resolve({
+            items,
+            total,
+          });
+        }, 1000);
+      });
     },
 
     editItem(item) {
+      var airlineName = "";
+      var nofoftrips;
+      var pname;
+
       this.editedIndex = this.passengers.indexOf(item);
-      this.editedItem = Object.assign({}, item);
+
+      for (var i = 0; i < item.airline.length; i++) {
+        console.log(item.airline[i].name);
+        airlineName = item.airline[i].name;
+        nofoftrips = item.trips;
+        pname = item.name;
+      }
+      console.log(airlineName, nofoftrips, pname);
+      const adata = {
+        airline: airlineName,
+        trips: nofoftrips,
+        name: pname,
+      };
+      this.editedItem = Object.assign({}, { adata });
+      console.log(this.editedItem);
+      console.log(adata.airline, adata.trips, adata.name);
       this.dialog = true;
     },
 
